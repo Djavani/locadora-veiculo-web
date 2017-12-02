063c900c38c45778eaa045bacaf348d674ec5b5f
djavani <djavanirgomes@gmail.com>
2017-12-02 16:53:53 -0200

    implementando a consulta e exclusao

--- src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -1,20 +1,33 @@
 package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
-
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-
 import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.util.jpa.Transactional;
 
-public class FabricanteDAO implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
+public class FabricanteDAO implements Serializable {
+
 	@Inject
 	private EntityManager em;
 	
 	public void salvar(Fabricante fabricante) {
-		em.persist(fabricante);		
+		em.persist(fabricante);
 	}
 
+	@SuppressWarnings("unchecked")
+	public List<Fabricante> buscarTodos() {
+		return em.createQuery("from Fabricante").getResultList();
+	}
+
+	@Transactional
+	public void excluir(Fabricante fabricante) throws NegocioException {
+		Fabricante fabricanteTemp = em.find(Fabricante.class, fabricante.getCodigo());
+		
+		em.remove(fabricanteTemp);
+		em.flush();
+	}
+	
 }
